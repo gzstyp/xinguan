966fc7e26b5aac9e1fb458db55512235461d694a
gzstyp <444141300@qq.com>
2020-11-27 15:24:09 +0800

    数据库操作,仅支持app

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ import App from './App';
 import store from './store/index.js';
 import exports from './resources/api/exports.js';
 Vue.prototype.$api = exports;
-
+//#ifdef APP-PLUS
 import release from './resources/db/release.js';
 Vue.prototype.$db = release;
-
+//#endif
 Vue.config.productionTip = false;
 App.mpType = 'app';
 // 检查是否已登录，参数 backpage : 登录后返回的页面; backtype : 打开页面的类型[1 : redirectTo 2 : switchTab];parentPage上一级页面;parentType是返回类型;
